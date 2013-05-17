59fe4fce4ba449ea3290d8b8f7196e3568e4323c
Adam Hull <adam@hmlad.com>
2013-05-17 01:48:59 -0700

    Intercept errors in jQuery and Backbone event listeners

---
+++ stackbone.js
@@ -0,0 +1,51 @@
+(function (root) {
+  var Stackbone;
+
+  // Support CommmonJS or global namespace
+  if (typeof exports !== 'undefined') {
+    Stackbone = exports;
+  } else {
+    Stackbone = root.Stackbone = {};
+  }
+
+  function defaultOnError (err) {
+    console.log(err && err.stack);
+  }
+
+  Stackbone.start = function (options) {
+    var Backbone, jQuery, onError;
+
+    options = options || {};
+    Backbone = options.Backbone || root.Backbone;
+    jQuery = options.jQuery || root.jQuery;
+    onError = options.onError || defaultOnError;
+
+    function interceptErrors (fn) {
+      return function() {
+        try {
+          fn.apply(this, arguments);
+        } catch (err) {
+          if(err && !err.intercepted) {
+            err.intercepted = true;
+            onError(err);
+          }
+          throw err;
+        }
+      };
+    }
+
+    // Intercept errors in Backbone event listeners
+    Backbone.Router.prototype.trigger =
+    Backbone.Model.prototype.trigger =
+    Backbone.View.prototype.trigger =
+    Backbone.Events.trigger = interceptErrors(Backbone.Events.trigger);
+
+    // Intercept errors in jQuery event listeners including ajax
+    var OriginalCallbacks = jQuery.Callbacks;
+    jQuery.Callbacks = function () {
+      var callbacks = OriginalCallbacks.apply(this, arguments);
+      callbacks.fireWith = interceptErrors(callbacks.fireWith);
+      return callbacks;
+    };
+  };
+})(this);
--- test/test.js
+++ test/test.js
@@ -1,6 +1,30 @@
 test = require('tape');
+Backbone = require('backbone');
+Stackbone = require('..');
 
-test('boilerplate', function (t) {
-  t.plan(1);
-  t.ok(false);
+var onError = sinon.spy();
+Stackbone.start({
+  Backbone: Backbone,
+  jQuery: jQuery,
+  onError: onError
+});
+
+test('Stackbone', function (t) {
+  t.test('reports errors in model event listeners', function (t) {
+    var model = new Backbone.Model({a: 1});
+    model.on('change', function() {
+      throw new Error('panic');
+    });
+
+    try {
+      model.set({a: 2});
+    } catch (err) {
+      // expected, swallow it
+    }
+
+    t.ok(onError.called);
+    t.end();
+  });
+
+  t.end();
 });
